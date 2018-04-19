73b4ad5c57ef2b63284b6bf572621b265ab9a357
Anton Volkov <vadrx@yandex.ru>
2018-04-19 15:06:09 +0300

    Initial Commit

---
+++ newPixelCL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  newPixelCL
+//
+//  Created by Anton Volkov on 19/04/2018.
+//  Copyright © 2018 Anton Volkov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
